892e214ed6314c316ab6e4d6cc837c49c03f25b2
RafaelScarpelli <rafaelscarpelli715@gmail.com>
2024-05-03 13:29:45 -0300

    Game panel and Game window created

---
+++ src/main/Game.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Game {
+
+	private GameWindow gamewindow;
+	private GamePanel gamepanel;
+	
+	public Game() {
+		gamepanel = new GamePanel();
+		gamewindow = new GameWindow(gamepanel);
+	}
+}
