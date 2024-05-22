341dac823423aad91cb3d14c1232e95a656915b5
RafaelScarpelli <rafaelscarpelli715@gmail.com>
2024-05-22 15:19:42 -0300

    Details

--- src/main/Game.java
+++ src/main/Game.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 import entities.Player;
 import levels.LevelManager;
 
-public class Game implements Runnable{
+public class Game implements Runnable {
 
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
@@ -14,42 +14,42 @@ public class Game implements Runnable{
 	private final int UPS_SET = 200;
 	private Player player;
 	private LevelManager levelManager;
-	
-	public final static int TILE_DEFALUT_SIZE = 32;
-	public final static float SCALE = 1f;
+
+	public final static int TILES_DEFAULT_SIZE = 32;
+	public final static float SCALE = 1.0f;
 	public final static int TILES_IN_WIDTH = 26;
 	public final static int TILES_IN_HEIGHT = 14;
-	public final static int TILES_SIZE = (int) (TILE_DEFALUT_SIZE * SCALE);
+	public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
 	public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
 	public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
-	
+
 	public Game() {
 		initClasses();
-		
+
 		gamePanel = new GamePanel(this);
 		gameWindow = new GameWindow(gamePanel);
-		gamePanel.setFocusable(true);
 		gamePanel.requestFocus();
-		
+
 		startGameLoop();
 	}
-	
-	public void initClasses() {
+
+	private void initClasses() {
 		levelManager = new LevelManager(this);
-		player = new Player(200, 200, (int) (64 * SCALE), (int) (40 * SCALE));
+		player = new Player(150, 200, (int) (64 * SCALE), (int) (40 * SCALE));
 		player.loadlvlData(levelManager.getCurrentLevel().getLevelData());
+
 	}
 
-	public void startGameLoop() {
+	private void startGameLoop() {
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
-	
+
 	public void update() {
-		player.update();
 		levelManager.update();
+		player.update();
 	}
-	
+
 	public void render(Graphics g) {
 		levelManager.draw(g);
 		player.render(g);
@@ -57,52 +57,53 @@ public class Game implements Runnable{
 
 	@Override
 	public void run() {
-		
+
 		double timePerFrame = 1000000000.0 / FPS_SET;
 		double timePerUpdate = 1000000000.0 / UPS_SET;
-		
+
 		long previousTime = System.nanoTime();
-		
+
 		int frames = 0;
-		int updates = 0;		
+		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
-		
+
 		double deltaU = 0;
 		double deltaF = 0;
-		
-		while(true) {
-			
+
+		while (true) {
 			long currentTime = System.nanoTime();
-			
+
 			deltaU += (currentTime - previousTime) / timePerUpdate;
 			deltaF += (currentTime - previousTime) / timePerFrame;
 			previousTime = currentTime;
-			
-			if(deltaU >= 1) {
+
+			if (deltaU >= 1) {
 				update();
 				updates++;
 				deltaU--;
 			}
-			
-			if(deltaF >= 1) {
+
+			if (deltaF >= 1) {
 				gamePanel.repaint();
-				deltaF--;
 				frames++;
+				deltaF--;
 			}
-			
-			if(System.currentTimeMillis() - lastCheck >= 1000) {
+
+			if (System.currentTimeMillis() - lastCheck >= 1000) {
 				lastCheck = System.currentTimeMillis();
 				System.out.println("FPS: " + frames + " | UPS: " + updates);
 				frames = 0;
 				updates = 0;
+
 			}
-			
 		}
+
 	}
-	
+
 	public void windowFocusLost() {
 		player.resetDirBooleans();
 	}
+
 	public Player getPlayer() {
 		return player;
 	}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,12 +2,9 @@ package main;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
-
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
-
 import static main.Game.GAME_HEIGHT;
 import static main.Game.GAME_WIDTH;
 
@@ -15,11 +12,10 @@ public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private Game game;
-	
-	public GamePanel(Game game){
+
+	public GamePanel(Game game) {
 		mouseInputs = new MouseInputs(this);
 		this.game = game;
-		
 		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
@@ -27,16 +23,14 @@ public class GamePanel extends JPanel {
 	}
 
 	private void setPanelSize() {
-		
 		Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
 		setPreferredSize(size);
-		
 	}
-	
+
 	public void updateGame() {
-		
+
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		game.render(g);
@@ -45,4 +39,5 @@ public class GamePanel extends JPanel {
 	public Game getGame() {
 		return game;
 	}
+
 }
