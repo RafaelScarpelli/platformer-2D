2dce2968e6ae1ab6940a6ee8e2be59aee168675b
RafaelScarpelli <rafaelscarpelli715@gmail.com>
2024-06-03 13:03:55 -0300

    Creating replay, unpause and home buttons

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -14,7 +14,7 @@ public class Playing extends State implements Statemethods {
 	private Player player;
 	private LevelManager levelManager;
 	private PauseOverlay pauseOverlay;
-	private boolean paused = true;
+	private boolean paused = false;
 
 	public Playing(Game game) {
 		super(game);
@@ -25,15 +25,17 @@ public class Playing extends State implements Statemethods {
 		levelManager = new LevelManager(game);
 		player = new Player(150, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
 		player.loadlvlData(levelManager.getCurrentLevel().getLevelData());
-		pauseOverlay = new PauseOverlay();
+		pauseOverlay = new PauseOverlay(this);
 	}
 
 	@Override
 	public void update() {
-		levelManager.update();
-		player.update();
-
-		pauseOverlay.update();
+		if (!paused) {
+			levelManager.update();
+			player.update();
+		} else {
+			pauseOverlay.update();
+		}
 	}
 
 	@Override
@@ -41,7 +43,9 @@ public class Playing extends State implements Statemethods {
 		levelManager.draw(g);
 		player.render(g);
 
-		pauseOverlay.draw(g);
+		if(paused) {
+			pauseOverlay.draw(g);
+		}
 	}
 
 	@Override
@@ -85,8 +89,8 @@ public class Playing extends State implements Statemethods {
 		case KeyEvent.VK_SPACE:
 			player.setJump(true);
 			break;
-		case KeyEvent.VK_BACK_SPACE:
-			Gamestate.state = Gamestate.MENU;
+		case KeyEvent.VK_ESCAPE:
+			paused = !paused;
 			break;
 		}
 
@@ -106,6 +110,10 @@ public class Playing extends State implements Statemethods {
 			break;
 		}
 
+	}
+	
+	public void unpauseGame() {
+		paused = false;
 	}
 
 	public void windowFocusLost() {
