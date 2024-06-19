365d4466a460a19a3e71eda8378127646a8a0bf6
RafaelScarpelli <rafaelscarpelli715@gmail.com>
2024-06-19 13:52:10 -0300

    Creating Enemy Hitbox

--- src/entities/Crabby.java
+++ src/entities/Crabby.java
@@ -1,21 +1,48 @@
 package entities;
 
-import static utilz.Constants.EnemyConstants.*;
-import static utilz.Constants.Directions.*;
+import static utilz.Constants.Directions.RIGHT;
+import static utilz.Constants.EnemyConstants.ATTACK;
+import static utilz.Constants.EnemyConstants.CRABBY;
+import static utilz.Constants.EnemyConstants.CRABBY_HEIGHT;
+import static utilz.Constants.EnemyConstants.CRABBY_WIDTH;
+import static utilz.Constants.EnemyConstants.IDLE;
+import static utilz.Constants.EnemyConstants.RUNNING;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 
 import main.Game;
 
 public class Crabby extends Enemy {
 
+	// AttackBox
+	private Rectangle2D.Float attackBox;
+	private int attackBoxOffsetX;
+
 	public Crabby(float x, float y) {
 		super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
 		initHitbox(x, y, (int) (22 * Game.SCALE), (int) (19 * Game.SCALE));
+		initAttackBox();
+		
+	}
 
+	private void initAttackBox() {
+		attackBox = new Rectangle2D.Float(x, y, (int) (82 * Game.SCALE), (int) (19 * Game.SCALE));
+		attackBoxOffsetX = (int) (Game.SCALE * 30);
+		
 	}
 
 	public void update(int[][] lvlData, Player player) {
 		updateMove(lvlData, player);
 		updateAnimationTick();
+		updateAttackBox();
+
+	}
+
+	private void updateAttackBox() {
+		attackBox.x = hitbox.x - attackBoxOffsetX;
+		attackBox.y = hitbox.y;
 
 	}
 
@@ -43,6 +70,11 @@ public class Crabby extends Enemy {
 
 	}
 	
+	public void drawAttackBox(Graphics g, int xLvlOffset) {
+		g.setColor(Color.red);
+		g.drawRect((int) (attackBox.x - xLvlOffset), (int) attackBox.y, (int) attackBox.width, (int) attackBox.height);
+	}
+
 	public int flipX() {
 		if (walkDir == RIGHT)
 			return width;
